22362805de217f1f7b4b4fad37cabf5bc9629010
alain <alain.formacion.cursos@gmail.com>
2019-12-12 14:46:27 +0100

    Update Principal.java
    soft resset

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,10 @@
 public class Principal {
 	public static void main(String[] args) {
 		//programa mio
+<<<<<<< HEAD
 		//eso es lo que tu te crees. Ahora es mio, jajajajaja
 		 
+=======
+>>>>>>> parent of a08edad... Usurpación de propietario
 	}
 }
